383de8fb1152c30bd42d91d4b8e5ad902fe91db4
Awinash <mail.awinashjaiswal@gmail.com>
2019-11-10 18:55:43 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Mondrian } from './mondrian';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'testApp';
+export class AppComponent implements OnInit {
+  mdrn: any;
+  msg: any;
+  ngOnInit() {
+    this.mdrn = new Mondrian('b');
+    this.mdrn.onMsg('topic2', msg => {
+      this.msg = msg;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiblingComponent } from './sibling/sibling.component';
+import { MessengerService } from './messenger.service';
+import { Sib2Component } from './sib2/sib2.component';
+import { Sib3Component } from './sib3/sib3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiblingComponent,
+    Sib2Component,
+    Sib3Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MessengerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
