8954f06b482731bc12886477337e7be1552f4c6a
Awinash <mail.awinashjaiswal@gmail.com>
2019-11-10 19:01:14 +0530

    test failure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'testApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testApp');
+    expect(app.title).toEqual('test');
   });
 
   it('should render title in a h1 tag', () => {
